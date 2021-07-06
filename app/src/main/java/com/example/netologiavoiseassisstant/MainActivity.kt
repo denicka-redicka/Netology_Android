9f8f72002dce4cdd355315613573883f4cd6d504
denicka-redicka <minato01.98@gmail.com>
2021-07-06 23:37:45 +0300

    The first task is done

---
+++ app/src/main/java/com/example/netologiavoiseassisstant/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.netologiavoiseassisstant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity1"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "start of onCreate function")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Ivan"
+        val surname = "Ivanov"
+        var age = 37
+        var height = 172.02
+        val summary = "name: $name surname: $surname age: $age height: $height "
+
+        val output : TextView = findViewById(R.id.output)
+        //findViewById<TextView>(R.id.output).text=summary
+        output.text = summary
+        Log.d(TAG, "end of onCreate function")
+    }
+}
